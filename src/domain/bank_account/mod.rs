bbd3ddf32a8751de712a6974394ef4d7cd0d9e51
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2023-02-28 00:01:19 +0100

    Added bank account aggregate to domain

---
+++ src/domain/bank_account/mod.rs
@@ -0,0 +1,8 @@
+pub mod aggregate;
+pub mod commands;
+pub mod events;
+
+// Re-exports
+pub use aggregate::*;
+pub use commands::*;
+pub use events::*;
--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,8 +1,10 @@
+mod bank_account;
 pub mod project;
 pub mod task;
 pub mod todo;
 
 // Re-exports
+pub use bank_account::*;
 pub use project::*;
 pub use task::*;
 pub use todo::*;
