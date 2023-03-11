9855c768fc60175c8e2effecdeae9df8eef6da4e
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2023-03-11 19:18:35 +0100

    Add authorisation middleware

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,9 +3,27 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from '@auth/core/providers/github';
 import GoogleProvider from '@auth/core/providers/google';
+import { sequence } from '@sveltejs/kit/hooks';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { GITHUB_ID, GITHUB_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private"
 
-export const handle = SvelteKitAuth({
+async function authorization({ event, resolve }) {
+  // Protect any routes under /authenticated
+  if (event.url.pathname.startsWith('/authenticated')) {
+    const session = await event.locals.getSession();
+    if (!session) {
+      throw redirect(303, '/auth/signin');
+    }
+  }
+
+  // If the request is still here, just proceed as normally
+  const result = await resolve(event, {
+    transformPageChunk: ({ html }) => html
+  });
+  return result;
+}
+
+export const handle: Handle = sequence(SvelteKitAuth({
   providers: [
     GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
     GoogleProvider({
@@ -13,4 +31,4 @@ export const handle = SvelteKitAuth({
       clientSecret: GOOGLE_CLIENT_SECRET
     })
   ]
-});
+}), authorization);
